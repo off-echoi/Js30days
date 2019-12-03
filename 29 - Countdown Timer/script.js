f89e220dc1392cfa0c125369ffc1a314d0479562
ecchoi <off.echoi@gmail.com>
2019-12-03 11:04:21 +0900

    Day 29

---
+++ 29 - Countdown Timer/script.js
@@ -0,0 +1,26 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left')
+
+ function timer(seconds){
+	const now = Date.now();
+	const then = now + seconds * 1000;
+	displayTimeLeft(seconds);
+
+	countdown = setInterval(() => {
+		const secondsLeft = Math.round((then - Date.now())/1000);
+		// 우리가 멈춰야 하는지 체크
+		if(secondsLeft < 0){
+			clearInterval(countdown);
+			return;
+		}
+		// 
+		displayTimeLeft(secondsLeft);
+	}, 1000);
+}
+
+function displayTimeLeft(seconds){
+	const minutes = Math.floor(seconds/60);
+	const remainderSeconds = seconds % 60;
+	const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+	timerDisplay.textContent = display
+};
